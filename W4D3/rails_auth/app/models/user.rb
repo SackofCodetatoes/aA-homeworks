6ad7cab186e586e8d67953a06cc62e5c3f3fc4d3
Jason Wong <jsnwong@ucdavis.edu>
2018-07-10 22:05:16 -0700

    Work on W4D3

---
+++ W4D3/rails_auth/app/models/user.rb
@@ -0,0 +1,36 @@
+require 'bcrypt'
+class User < ApplicationRecord
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: {message: "Password can't be blank"}
+  validates :password, length: { minimum: 8, allow_nil: true }
+  validates :session_token, presence: true, uniqueness: true
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def self.find_by_credentials
+    some_user = User.find_by(username: username)
+    return some_user if some_user && BCrypt::Password.new(some_user.password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+     SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+
+end
